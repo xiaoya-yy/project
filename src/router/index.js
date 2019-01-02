bd6c559801b1cd0d0d4c6199dc01bb95583d0c94
Your Name <993504791@qq.com>
2019-01-02 19:38:46 +0800

    初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      // 路由配置的path必须赋值
+      path: '/',
+      
+    }
+  ]
+})
